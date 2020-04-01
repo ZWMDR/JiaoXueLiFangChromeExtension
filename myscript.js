778430ece4913d03540e8b04a5af652df217ee4a
sky <171180575@smail.nju.edu.cn>
2020-04-01 20:03:47 +0800

    解决了一个bug

--- myscript.js
+++ myscript.js
@@ -1,10 +1,12 @@
 window.onhashchange = e => {
     if(location.hash==="#S-Lesson-index"){
         let src = chrome.runtime.getURL('catchHomework.js');
-        if(!document.querySelector(`head script[src="${src}"]`)) {
-            var s = document.createElement('script');
-            s.src = src;
-            document.getElementsByTagName('head')[0].appendChild(s);
+        let _script = document.querySelector(`head script[src="${src}"]`);
+        if(_script) {
+            _script.parentNode.removeChild(_script);
         }
+        var s = document.createElement('script');
+        s.src = src;
+        document.getElementsByTagName('head')[0].appendChild(s);
     }
 }
